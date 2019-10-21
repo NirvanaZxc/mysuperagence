c712e97f58e0c0c698c53845adf64b4c698b3dff
nirvana <xianchi.zhang@francetv.fr>
2019-10-21 17:51:03 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+	namespace App\Controller;
+
+
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
+	use Twig\Environment;
+
+	class HomeController
+	{
+		private $twig;
+
+		public function __construct(Environment $twig) {
+			$this->twig = $twig;
+		}
+
+		/**
+		 * @Route("/", name="home")
+		 */
+		public function index():Response {
+			return new Response($this->twig->render('pages/home.html.twig'));
+		}
+	}
