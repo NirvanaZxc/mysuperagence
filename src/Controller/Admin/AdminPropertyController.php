fcda59f9da78bb91f9132da7e3f5ee21bec4cb53
nirvana <xianchi.zhang@francetv.fr>
2019-10-28 16:34:57 +0100

    Form

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -12,7 +12,10 @@ namespace App\Controller\Admin;
 use App\Entity\Property;
 use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,11 +25,21 @@ class AdminPropertyController extends AbstractController
 	 * @var PropertyRepository
 	 */
 	private $repository;
+	/**
+	 * @var ObjectManager
+	 */
+	private $em;
 
-	public function __construct(PropertyRepository $repository)
+	/**
+	 * AdminPropertyController constructor.
+	 * @param PropertyRepository $repository
+	 * @param ObjectManager $em
+	 */
+	public function __construct(PropertyRepository $repository, ObjectManager $em)
 	{
 		$this->repository = $repository;
 
+		$this->em = $em;
 	}
 
 	/**
@@ -41,13 +54,45 @@ class AdminPropertyController extends AbstractController
 	}
 
 	/**
-	 * @Route("/admin/{id}", name="admin.property.edit")
-	 * @param \App\Entity\Property $property
-	 * @return \Symfony\Component\HttpFoundation\Response
+	 * @Route("/admin/property/create", name="admin.property.new")
+	 * @param Request $request
+	 * @return RedirectResponse
 	 */
-	public function edit(Property $property): Response
+	public function new(Request $request)
+	{
+		$property = new Property();
+		$form = $this->createForm(PropertyType::class, $property);
+		$form->handleRequest($request);
+		if($form->isSubmitted() && $form->isValid())
+		{
+			$this->em->persist($property);
+			$this->em->flush();
+
+			return $this->redirectToRoute('admin.property.index');
+		}
+
+		return $this->render("admin/property/new.html.twig", [
+			'property' => $property,
+			'form' => $form->createView()
+		]);
+	}
+
+	/**
+	 * @Route("/admin/property/{id}", name="admin.property.edit")
+	 * @param Property $property
+	 * @param Request $request
+	 * @return Response
+	 */
+	public function edit(Property $property, Request $request)
 	{
 		$form = $this->createForm(PropertyType::class, $property);
+		$form->handleRequest($request);
+		if($form->isSubmitted() && $form->isValid())
+		{
+			$this->em->flush();
+
+			return $this->redirectToRoute('admin.property.index');
+		}
 		return $this->render("admin/property/edit.html.twig", [
 			'property' => $property,
 			'form' => $form->createView()
--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Property;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,7 +20,9 @@ class PropertyType extends AbstractType
             ->add('bedrooms')
             ->add('floor')
             ->add('price')
-            ->add('heat')
+            ->add('heat', ChoiceType::class, [
+            	'choices' => $this->getChoices()
+            ])
             ->add('city')
             ->add('address')
             ->add('postal_code')
@@ -34,5 +37,17 @@ class PropertyType extends AbstractType
             'data_class' => Property::class,
             'translation_domain' => 'forms'
         ]);
+    }
+
+	public function getChoices()
+    {
+    	$heat = Property::HEAT;
+    	$output = [];
+    	foreach ($heat as $k => $v)
+	    {
+	    	$output[$v] = $k;
+	    }
+
+    	return $output;
     }
 }
